16a00c3e0e378cf9b49125e732ea35a8083c7248
Ajay Singh <daltavida@gmail.com>
2022-01-28 14:00:00 +0100

    Create Card component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,3 @@
+const Card = (props) => {};
+
+export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from "../UI/Card";
+
 const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
