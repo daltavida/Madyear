87bb583809c4736ff579b96d94b45797551ba80f
Ajay Singh <daltavida@gmail.com>
2022-01-28 14:00:00 +0100

    Create function to handle Add User button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,15 @@
 const AddUser = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type="text" />
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
     </form>
   );
 };
