12933a45b5d897857cb789a56fce42c74fd487fe
Ajay Singh <daltavida@gmail.com>
2022-01-29 14:00:00 +0100

    Create and Import CSS for card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,3 +1,7 @@
-const Card = (props) => {};
+import styles from "./Card.module.css";
+
+const Card = (props) => {
+  return <div className={styles.card}>{props.children}</div>;
+};
 
 export default Card;
