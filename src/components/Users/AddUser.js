5c1df8789767c3f127298e4ac9ec39365d7a3369
Ajay Singh <daltavida@gmail.com>
2022-01-30 14:00:00 +0100

    Create states and handlers for form fields

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername);
+    console.log(enteredAge);
+  };
+
+  const usernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameHandler} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
