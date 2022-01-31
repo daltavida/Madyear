f1ee709c7981579f7175b6b848649d8bb89b0bba
Ajay Singh <daltavida@gmail.com>
2022-01-31 14:00:00 +0100

    Create CSS for userrs list

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
 const UsersList = (props) => {
   return (
-    <ul>
-      {props.users.map((user) => {
-        return (
-          <li>
-            {user.name} ({user.age} years old)
-          </li>
-        );
-      })}
-    </ul>
+    <Card>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li>
+              {user.name} ({user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
   );
 };
 
