5fff635327c2440cd91a8a3cd530e2cf418991f0
Ajay Singh <daltavida@gmail.com>
2022-02-01 14:00:00 +0100

    Use userslist in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList />
     </div>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+import styles from "./UsersList.module.css";
 import Card from "../UI/Card";
 
 const UsersList = (props) => {
   return (
-    <Card>
+    <Card className={styles.users}>
       <ul>
         {props.users.map((user) => {
           return (
