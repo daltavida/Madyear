1ee37e42b056e5019af45fdd0a94b209183c9bd9
Ajay Singh <daltavida@gmail.com>
2022-02-02 14:00:00 +0100

    Manage state for users list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
   return (
     <div>
       <AddUser />
-      <UsersList users={[]} />
+      <UsersList users={usersList} />
     </div>
   );
 }
